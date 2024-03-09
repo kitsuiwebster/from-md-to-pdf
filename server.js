669559598e1e6b9cb87157e9b21785dec293862f
kitsuiwebster <raphmartin1999@gmail.com>
2024-03-09 17:46:39 +0100

    fix: deleted objects

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const port = 4005;
-
-app.use('/pictures', express.static('/home/ash/kitsui/pdf-gen-js/pictures'));
-
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
-});
